9d9a371ece9f7b836acbe976d54dc18a57388b49
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-03-30 10:59:56 +0200

    Initial push

---
+++ src/test/java/cookbook/stage/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cookbook.stage.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
