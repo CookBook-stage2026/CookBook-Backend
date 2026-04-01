6ef16f41817cb990f1f464add163c90315a83d84
Jurgen.P <jurgen.potter@student.kdg.be>
2026-04-01 10:51:56 +0200

    fix: #6 Removed ingredients

--- src/main/java/cookbook/stage/backend/ingredient/infrastructure/jpa/JpaIngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package cookbook.stage.backend.ingredient.infrastructure.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface JpaIngredientRepository extends JpaRepository<JpaIngredientEntity, UUID> {
-}
