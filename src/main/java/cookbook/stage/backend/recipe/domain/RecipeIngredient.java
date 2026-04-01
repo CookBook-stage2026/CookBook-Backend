c2bfe8cad7b4c4dbb3cbfc486a83574db5bc0d71
Jurgen.P <jurgen.potter@student.kdg.be>
2026-04-01 10:51:56 +0200

    feat: #6 Finished tests

--- src/main/java/cookbook/stage/backend/recipe/domain/RecipeIngredient.java
+++ src/main/java/cookbook/stage/backend/recipe/domain/RecipeIngredient.java
@@ -1,7 +1,6 @@
 package cookbook.stage.backend.recipe.domain;
 
 import cookbook.stage.backend.ingredient.shared.IngredientId;
-import cookbook.stage.backend.recipe.shared.BaseQuantityInvalidException;
 import cookbook.stage.backend.recipe.shared.RecipeId;
 import org.jmolecules.ddd.annotation.ValueObject;
 
@@ -13,7 +12,7 @@ public record RecipeIngredient(RecipeId recipeId, IngredientId ingredientId, boo
         Objects.requireNonNull(recipeId, "Recipe id cannot be null!");
         Objects.requireNonNull(ingredientId, "Ingredient id cannot be null!");
         if (baseQuantity <= 0) {
-            throw new BaseQuantityInvalidException("Base quantity must be greater than 0!");
+            throw new IllegalArgumentException("Base quantity must be greater than 0!");
         }
     }
 }
