4fea5027918e327a04e85bded2e0ad178dddfd2d
Jurgen.P <jurgen.potter@student.kdg.be>
2026-04-01 10:51:56 +0200

    feat: #6 Finished tests

--- src/main/java/cookbook/stage/backend/recipe/shared/BaseQuantityInvalidException.java
+++
@@ -1,7 +0,0 @@
-package cookbook.stage.backend.recipe.shared;
-
-public class BaseQuantityInvalidException extends RuntimeException {
-    public BaseQuantityInvalidException(String message) {
-        super(message);
-    }
-}
--- src/main/java/cookbook/stage/backend/shared/api/GlobalExceptionHandler.java
+++ src/main/java/cookbook/stage/backend/shared/api/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package cookbook.stage.backend.shared.api;
 
-import cookbook.stage.backend.recipe.shared.BaseQuantityInvalidException;
 import cookbook.stage.backend.shared.domain.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,57 +7,23 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.Instant;
-import java.util.List;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(BaseQuantityInvalidException.class)
-    public ResponseEntity<ApiError>  handleBaseQuantityInvalidException(BaseQuantityInvalidException e) {
-        ApiError body = new ApiError(
-                HttpStatus.BAD_REQUEST.value(),
-                "Base quantity is invalid.",
-                e.getMessage(),
-                List.of(),
-                Instant.now()
-        );
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    public record ErrorResponse(String message) {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiError>  handleNotFoundException(NotFoundException e) {
-        ApiError body = new ApiError(
-                HttpStatus.NOT_FOUND.value(),
-                "Not Found",
-                e.getMessage(),
-                List.of(),
-                Instant.now()
-        );
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError>  handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        ApiError body = new ApiError(
-                HttpStatus.BAD_REQUEST.value(),
-                "Not Found",
-                e.getMessage(),
-                List.of(),
-                Instant.now()
-        );
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleAll(Exception ex) {
-        ApiError body = new ApiError(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "Internal Server Error",
-                "Something went wrong: " + ex.getMessage(),
-                List.of(),
-                Instant.now()
-        );
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    public ResponseEntity<ErrorResponse> handleAll(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getMessage()));
     }
-
 }
