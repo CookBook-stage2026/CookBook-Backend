8e5f27eb39b28bf925986b04aa41cecbb28f67f1
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-04-28 09:44:49 +0200

    Fix: #14 fix bug

--- src/main/java/cookbook/stage/backend/repository/jpa/schedule/JpaWeekScheduleEntity.java
+++ src/main/java/cookbook/stage/backend/repository/jpa/schedule/JpaWeekScheduleEntity.java
@@ -8,8 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.util.ArrayList;
@@ -23,8 +23,8 @@ public class JpaWeekScheduleEntity {
     @Id
     private UUID id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private JpaUserEntity user;
 
     @OneToMany(
