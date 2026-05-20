cd4519f2b14fd911c27e8cfc3f57eb48815813b3
Robbe Van Mele <robbe.vanmele@student.kdg.be>
2026-05-20 12:59:02 +0200

    Fix: #77 test pipeline

--- application/src/main/java/be/xplore/cookbook/security/UserExistenceJwtValidator.java
+++ application/src/main/java/be/xplore/cookbook/security/UserExistenceJwtValidator.java
@@ -1,5 +1,6 @@
 package be.xplore.cookbook.security;
 
+import be.xplore.cookbook.core.domain.exception.UserNotFoundException;
 import be.xplore.cookbook.core.domain.user.User;
 import be.xplore.cookbook.core.domain.user.UserId;
 import be.xplore.cookbook.core.domain.user.command.FindUserByIdQuery;
@@ -33,6 +34,8 @@ public class UserExistenceJwtValidator implements OAuth2TokenValidator<Jwt> {
             if (user != null) {
                 return OAuth2TokenValidatorResult.success();
             }
+        } catch (UserNotFoundException _) {
+            // Ignored
         } catch (RuntimeException _) {
             throw new OAuth2Exception("Invalid JWT subject");
         }
